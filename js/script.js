3e943ed1e9cd0080c937b7e3f8f85b5ad25733bc
Arturs Anikins <xyzdelete@protonmail.com>
2023-02-03 13:31:14 +0200

    Add Image Optimizations

--- js/script.js
+++ js/script.js
@@ -11,11 +11,6 @@ console.log(h1);
 //     h1.style.padding = "5rem";
 // });
 
-// Set current year
-const yearEl = document.querySelector(".year");
-const currentYear = new Date().getFullYear();
-yearEl.textContent = currentYear;
-
 // Make mobile navigation work
 const btnNavEl = document.querySelector(".btn-mobile-nav");
 const headerEl = document.querySelector(".header");
